cde29af46823d4075a7e57e6e9948ff93641d089
gholway <131705574+gholway@users.noreply.github.com>
2024-01-18 18:06:30 -0700

    w02work

---
+++ scripts/w02-task.js
@@ -0,0 +1,29 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullName = 'Gavin Holway';
+let currentYear;
+let profilePicture = 'images/Capture.jpg';
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imageElement = profilePicture;
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+currentYear.textContent = datetime.now;
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute.alt = 'profile image of Gavin Holway';
+
+
+/* Step 5 - Array */
+
+
+
+
+
+
